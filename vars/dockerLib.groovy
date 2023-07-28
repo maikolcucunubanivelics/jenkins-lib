5bed204593e79fce032569fe3c46941d4605e61f
mcucunuba_coopeuch <mcucunuba.externo@coopeuch.cl>
2023-07-28 07:43:57 -0500

    cambiando libreria

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -2,6 +2,10 @@ def clonar(Map params){
     echo "Clonando ${params.nombreRepo}"
 }
 
+def construyendo(Map params){
+    echo "construyendo ${params.nombreRepo}"
+}
+
 def publicando(Map params){
     echo "publicando ${params.version}"
 }
---
+++ vars/dockerPipeline.groovy
@@ -0,0 +1,29 @@
+def call(Map pipelineParams){
+    pipeline {
+        agent any
+
+        stages {
+            stage('clona') {
+                steps {
+                    script {
+                        dockerLib.clonar(nombreRepo: "matias")
+                    }
+                }
+            }
+            stage('compilando') {
+                steps {
+                    echo 'Mvn clean installl ...............'
+                    dockerLib.construyendo(nombreRepo: "matias")
+                }
+            }
+            stage('desplegando') {
+                steps {
+                script {
+                        dockerLib.publicando(version: "1.0")
+                    }
+                }
+            }
+        }
+    }
+
+}
